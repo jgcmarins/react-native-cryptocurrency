a5bedacf79e90960996468bb4d088f23869b430e
João Marins <jgcmarins@gmail.com>
2018-02-01 08:50:18 -0200

    Add fetch

--- src/components/lists/AllCoinsList.js
+++ src/components/lists/AllCoinsList.js
@@ -4,10 +4,35 @@ import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 
 import CoinRow from '../coin/CoinRow';
+import Loading from '../common/Loading';
+import SampleText from '../common/SampleText';
 
-import { coins } from '../common/consts';
+// import { coins } from '../common/consts';
+
+const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=10';
 
 class AllCoinsList extends Component {
+
+  state = {
+    coins: [],
+    isFetching: true,
+    errorMessage: '',
+  };
+
+  requestCoins = async () => {
+    try {
+      const response = await fetch(url);
+      const coins = await response.json();
+      this.setState({ coins: coins });
+    } catch(error) {
+      this.setState({isFetching: false, errorMessage: error});
+      console.log('error: ', error);
+    }
+  };
+
+  componentDidMount() {
+    this.requestCoins();
+  }
 
   _keyExtractor = (item) => item.id;
 
@@ -19,12 +44,23 @@ class AllCoinsList extends Component {
   );
 
   render() {
-    return(
-      <FlatList
-        data={coins}
-        keyExtractor={this._keyExtractor}
-        renderItem={this._renderItem}
-      />
+    const { coins, isFetching, errorMessage } = this.state;
+
+    if (isFetching) {
+      return (
+        <Loading/>
+      );
+    }
+
+    return (
+      <View>
+      {coins.length === 0 ? <SampleText>{errorMessage}</SampleText> :
+        <FlatList
+          data={coins}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />}
+      </View>
     );
   }
 };
